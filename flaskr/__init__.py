e463ad87e0271fb82503b693293c4bd77801a2fa
valentina buoro <valentinabuoro4@gmail.com>
2025-04-06 20:54:16 +0100

    modified create_app function to allow Gunicorn pass multiple arguments

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 
 
-def create_app(test_config=None):
+def create_app(test_config=None,*args, **kwargs):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
